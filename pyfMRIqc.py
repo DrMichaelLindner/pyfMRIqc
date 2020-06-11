fix in calculation of relative movement

pyfMRIqc.py
@@ -483,16 +483,19 @@ def process(niifile, motionfile, maskthresh, maskniifile, outputdirectory, fname
             for ii in [0, 1, 2]:
                 rm[:, ii] = rm[:, ii] * 50
 
-        # absolute values
+        # create relative values
+        relrm = np.diff(rm, axis=0)
+
+        # create absolute values of relative movement
+        relrm = np.absolute(relrm)
+
+        # absolute values of absolute movement
         rm = np.absolute(rm)
 
         # sum absolute values
         rmsum = np.sum(rm, axis=1)
 
-        # create relative values
-        relrm = np.diff(rm, axis=1)
-
-        # get thrtesholds:
+        # get thresholds:
         nrrm01 = relrm[np.where(relrm > .1)]
         nrrm05 = relrm[np.where(relrm > .5)]
         nrrmv = relrm[np.where(relrm > voxelsize)]
@@ -619,7 +622,7 @@ def process(niifile, motionfile, maskthresh, maskniifile, outputdirectory, fname
         text_file.write("Max relative Movement: " + str(np.max(relrm)) + "\n")
         text_file.write("Relative movements (>0.1mm): " + str(len(nrrm01)) + "\n")
         text_file.write("Relative movements (>0.5mm): " + str(len(nrrm05)) + "\n")
-        text_file.write("Relataive movements (>voxelsize): " + str(len(nrrmv)) + "\n")
+        text_file.write("Relative movements (>voxelsize): " + str(len(nrrmv)) + "\n")
 
         # add line to plot
         plt.plot(stats.zscore(rmsum), label='sum of relative movements')